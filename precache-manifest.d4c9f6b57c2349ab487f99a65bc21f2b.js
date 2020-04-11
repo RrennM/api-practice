7db0d54fab7474055fc85549d81d6596982fceb8
Tyson Hood <TysonHood@gmail.com>
2020-04-11 01:24:17 -0700

    Updates

---
+++ precache-manifest.d4c9f6b57c2349ab487f99a65bc21f2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19475e7bb10b84c3477bb92be0bc488",
+    "url": "/api-practice/index.html"
+  },
+  {
+    "revision": "b594cbeef9f432eac677",
+    "url": "/api-practice/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "d8080971945b77edcb91",
+    "url": "/api-practice/static/css/main.c1b47809.chunk.css"
+  },
+  {
+    "revision": "b594cbeef9f432eac677",
+    "url": "/api-practice/static/js/2.5f8f4464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/api-practice/static/js/2.5f8f4464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8080971945b77edcb91",
+    "url": "/api-practice/static/js/main.00492477.chunk.js"
+  },
+  {
+    "revision": "01686bdf3fb614790f98",
+    "url": "/api-practice/static/js/runtime-main.7b4c7721.js"
+  }
+]);
